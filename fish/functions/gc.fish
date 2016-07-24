fad134dbc1c513388874de2951e0c21dfb299596
Xia Kai(夏恺) <xiaket@gmail.com>
2016-07-24 20:47:50 +1000

    prevent "Adding new files: " from showing even though there's nothing to add.
    Signed-off-by: Xia Kai(夏恺) <xiaket@gmail.com>

--- fish/functions/gc.fish
+++ fish/functions/gc.fish
@@ -1,6 +1,6 @@
 function gc
-    set new_files (git status --short | grep '^??' | awk '{print $NF}' | sed "s/\n/ /g")
-    if test -n "new_files"
+    set __gc_new_files (git status --short | grep '^??' | awk '{print $NF}' | sed "s/\n/ /g")
+    if test -n "$__gc_new_files"
         echo "Adding new files: " $new_files
         for file in (git status --short | grep '^??' | awk '{print $NF}')
             read -p 'echo "Adding $file, sure?(y/n):"' -l choice
