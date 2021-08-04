bc36a8de21f2b40cf86892c6092c0585e1c35090
Kai Xia <kaix+github@fastmail.com>
2021-08-04 19:45:59 +1000

    do not use treesitter identation because better plugin.
    Signed-off-by: Kai Xia <kaix+github@fastmail.com>

--- nvim/lua/plugin-options.lua
+++ nvim/lua/plugin-options.lua
@@ -17,7 +17,8 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
   indent = {
-    enable = true
+    enable = true,
+    disable = {"python"},
   }
 }
 
