2f5a22a67aa499bb73bea1e2e5416a8ec7e1708f
Kai Xia <kaix+github@fastmail.com>
2021-10-02 20:44:57 +1000

    update snippets.

--- nvim/lua/snippets.lua
+++ nvim/lua/snippets.lua
@@ -17,10 +17,9 @@ luasnip.snippets = {
   python = {
     luasnip.snippet("exit", luasnip.text_node({"import sys", "sys.exit()"})),
     luasnip.snippet("shell", luasnip.text_node(
-      {"xiaket = locals()", "import os", "from prompt_toolkit.utils import DummyContext", "from ptpython.repl import PythonRepl, run_config", "repl = PythonRepl(get_globals=lambda : globals(), get_locals=lambda : xiaket, history_filename=os.path.expanduser('~/.ptpython_history'))", "run_config(repl)", "with DummyContext():", "    repl.run()"}
+      {"xiaket = locals()", "import os", "import logging", "logging.getLogger('parso').setLevel(logging.INFO)", "from prompt_toolkit.utils import DummyContext", "from ptpython.repl import PythonRepl, run_config", "repl = PythonRepl(get_globals=lambda : globals(), get_locals=lambda : xiaket, history_filename=os.path.expanduser('~/.ptpython_history'))", "run_config(repl)", "with DummyContext():", "    repl.run()"}
       )
     ),
-    luasnip.snippet("datetime", luasnip.text_node({"from datetime import datetime"})),
     luasnip.snippet("ifmain", luasnip.text_node({"if __name__ == '__main__':", "    main()"})),
   }
 }
