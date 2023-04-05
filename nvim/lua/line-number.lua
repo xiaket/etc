620e461242c3be895a93f528ebdb266dbce12a82
Kai Xia <kaix+github@fastmail.com>
2023-04-05 22:25:36 +1000

    stylua + add formatter for py and lua.

--- nvim/lua/line-number.lua
+++ nvim/lua/line-number.lua
@@ -1,76 +1,76 @@
 function relativeln(target)
-  if vim.b.lnstatus == nil then
-    vim.b.lnstatus = "number"
-  end
+	if vim.b.lnstatus == nil then
+		vim.b.lnstatus = "number"
+	end
 
-  if vim.b.lnstatus ~= "nonumber" then
-    if target == "number" then
-      vim.o.number = true
-      vim.o.relativenumber = false
-    else
-      vim.o.number = true
-      vim.o.relativenumber = true
-    end
-  else
-    vim.o.number = false
-  end
+	if vim.b.lnstatus ~= "nonumber" then
+		if target == "number" then
+			vim.o.number = true
+			vim.o.relativenumber = false
+		else
+			vim.o.number = true
+			vim.o.relativenumber = true
+		end
+	else
+		vim.o.number = false
+	end
 end
 
 -- Show relative line number when in command mode and absolute line number in edit mode
 local _group = vim.api.nvim_create_augroup("LineNumber", { clear = true })
 vim.api.nvim_create_autocmd("InsertEnter", {
-  pattern = "*",
-  callback = function()
-    relativeln("number")
-  end,
-  once = true,
-  group = _group,
+	pattern = "*",
+	callback = function()
+		relativeln("number")
+	end,
+	once = true,
+	group = _group,
 })
 vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = "*",
-  callback = function()
-    relativeln("relativenumber")
-  end,
-  once = true,
-  group = _group,
+	pattern = "*",
+	callback = function()
+		relativeln("relativenumber")
+	end,
+	once = true,
+	group = _group,
 })
 vim.api.nvim_create_autocmd("FocusLost", {
-  pattern = "*",
-  callback = function()
-    relativeln("number")
-  end,
-  once = true,
-  group = _group,
+	pattern = "*",
+	callback = function()
+		relativeln("number")
+	end,
+	once = true,
+	group = _group,
 })
 vim.api.nvim_create_autocmd("CursorMoved", {
-  pattern = "*",
-  callback = function()
-    relativeln("relativenumber")
-  end,
-  once = true,
-  group = _group,
+	pattern = "*",
+	callback = function()
+		relativeln("relativenumber")
+	end,
+	once = true,
+	group = _group,
 })
 
 function toggleln()
-  if vim.b.lnstatus == nil then
-    vim.b.lnstatus = "number"
-  end
+	if vim.b.lnstatus == nil then
+		vim.b.lnstatus = "number"
+	end
 
-  if vim.b.lnstatus == "number" then
-    vim.o.number = false
-    vim.o.relativenumber = false
-    vim.b.lnstatus = "nonumber"
-  else
-    vim.o.number = true
-    vim.o.relativenumber = true
-    vim.b.lnstatus = "number"
-  end
+	if vim.b.lnstatus == "number" then
+		vim.o.number = false
+		vim.o.relativenumber = false
+		vim.b.lnstatus = "nonumber"
+	else
+		vim.o.number = true
+		vim.o.relativenumber = true
+		vim.b.lnstatus = "number"
+	end
 end
 
 -- Use Ctrl-L to toggle the line number display.
 vim.api.nvim_set_keymap(
-  "",
-  "<C-L>",
-  ':lua toggleln()<CR>:lua require"gitsigns".toggle_signs()<CR>',
-  { noremap = true, silent = true }
+	"",
+	"<C-L>",
+	':lua toggleln()<CR>:lua require"gitsigns".toggle_signs()<CR>',
+	{ noremap = true, silent = true }
 )
--- nvim/lua/opts/snippets.lua
+++ nvim/lua/opts/snippets.lua
@@ -1,33 +1,30 @@
 local luasnip = require("luasnip")
 
 local function get_date(_, _, format)
-  return os.date(format)
+	return os.date(format)
 end
 
 luasnip.add_snippets("all", {
-  luasnip.snippet(
-    "dtime",
-    luasnip.function_node(get_date, {}, { user_args = { "%Y-%m-%d %H:%M" } })
-  ),
-  luasnip.snippet("date", luasnip.function_node(get_date, {}, { user_args = { "%Y-%m-%d" } })),
+	luasnip.snippet("dtime", luasnip.function_node(get_date, {}, { user_args = { "%Y-%m-%d %H:%M" } })),
+	luasnip.snippet("date", luasnip.function_node(get_date, {}, { user_args = { "%Y-%m-%d" } })),
 }, { key = "all" })
 
 luasnip.add_snippets("python", {
-  luasnip.snippet("exit", luasnip.text_node({ "import sys", "sys.exit()" })),
-  luasnip.snippet(
-    "shell",
-    luasnip.text_node({
-      "xiaket = locals()",
-      "import os",
-      "import logging",
-      "logging.getLogger('parso').setLevel(logging.INFO)",
-      "from prompt_toolkit.utils import DummyContext",
-      "from ptpython.repl import PythonRepl, run_config",
-      "repl = PythonRepl(get_globals=lambda : globals(), get_locals=lambda : xiaket, history_filename=os.path.expanduser('~/.ptpython_history'))",
-      "run_config(repl)",
-      "with DummyContext():",
-      "    repl.run()",
-    })
-  ),
-  luasnip.snippet("ifmain", luasnip.text_node({ "if __name__ == '__main__':", "    main()" })),
+	luasnip.snippet("exit", luasnip.text_node({ "import sys", "sys.exit()" })),
+	luasnip.snippet(
+		"shell",
+		luasnip.text_node({
+			"xiaket = locals()",
+			"import os",
+			"import logging",
+			"logging.getLogger('parso').setLevel(logging.INFO)",
+			"from prompt_toolkit.utils import DummyContext",
+			"from ptpython.repl import PythonRepl, run_config",
+			"repl = PythonRepl(get_globals=lambda : globals(), get_locals=lambda : xiaket, history_filename=os.path.expanduser('~/.ptpython_history'))",
+			"run_config(repl)",
+			"with DummyContext():",
+			"    repl.run()",
+		})
+	),
+	luasnip.snippet("ifmain", luasnip.text_node({ "if __name__ == '__main__':", "    main()" })),
 }, { key = "python" })
